86f536b9a7cc2a04ca8b724b0ce4bfdbf5d8edcb
s2vArya <shivams2varya@gmail.com>
2020-08-29 12:28:11 +0530

    first
    Initial Commit

---
+++ BoxShooter/Assets/Scripts/BasicTargetMover.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicTargetMover : MonoBehaviour
+{
+    [SerializeField]private float _rotateSpeed=180f;
+    [SerializeField]private float _motionMagnitude=0.1f;
+
+    public bool doSpin = true;
+    public bool doMotion = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (doSpin)
+        {
+            // Rotating gameObject
+            gameObject.transform.Rotate(Vector3.up * _rotateSpeed * Time.deltaTime);
+        }
+
+        if (doMotion)
+        {
+            //Moving game Object
+            gameObject.transform.Translate(Vector3.up * Mathf.Cos(Time.timeSinceLevelLoad) * _motionMagnitude);
+        }
+    }
+}
